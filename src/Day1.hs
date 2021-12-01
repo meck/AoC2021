84e6d963a0c49fd42dc6006ce75c6a3e9f1d4a8c
meck <Johan@meck.se>
2021-12-01 13:14:45 +0100

    Day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,7 +1,15 @@
 module Day1 (day01a, day01b) where
 
+import AoC.Parsing (int, run, sepNL)
+
+solveA :: Ord a => [a] -> Int
+solveA xs = length $ filter id $ zipWith (<) xs (tail xs)
+
+solveB :: (Ord a, Num a) => [a] -> Int
+solveB = solveA . foldr (zipWith (+)) (repeat 0) . take 3 . iterate tail
+
 day01a :: String -> String
-day01a = id
+day01a = show . solveA . run (sepNL int)
 
 day01b :: String -> String
-day01b = id
+day01b = show . solveB . run (sepNL int)
