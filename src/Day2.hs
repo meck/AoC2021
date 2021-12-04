4a523cbf8c613627f31c5069f100ecc351082745
meck <Johan@meck.se>
2021-12-04 15:51:01 +0100

    Day 2,3

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,7 +1,36 @@
 module Day2 (day02a, day02b) where
 
+import AoC.Parsing (Parser, int, run, sepNL, symbol)
+import Control.Applicative ((<|>))
+import Data.List (foldl')
+
+data Inst = Down Int | Up Int | Forward Int
+
+inst :: Parser Inst
+inst = for <|> down <|> up
+  where
+    for = Forward <$> (symbol "forward" >> int)
+    down = Down <$> (symbol "down" >> int)
+    up = Up <$> (symbol "up" >> int)
+
+type State = (Int, Int)
+
+stepA :: State -> Inst -> State
+stepA (h, d) i = case i of
+  (Down x) -> (h, d + x)
+  (Up x) -> (h, d - x)
+  (Forward x) -> (h + x, d)
+
+type StateWithAim = (Int, Int, Int)
+
+stepB :: StateWithAim -> Inst -> StateWithAim
+stepB (a, h, d) i = case i of
+  (Down x) -> (a + x, h, d)
+  (Up x) -> (a - x, h, d)
+  (Forward x) -> (a, h + x, d + (a * x))
+
 day02a :: String -> String
-day02a = id
+day02a = show . uncurry (*) . foldl' stepA (0, 0) . run (sepNL inst)
 
 day02b :: String -> String
-day02b = id
+day02b = show . (\(_, h, d) -> h * d) . foldl' stepB (0, 0, 0) . run (sepNL inst)
--- src/Day3.hs
+++ src/Day3.hs
@@ -1,7 +1,54 @@
 module Day3 (day03a, day03b) where
 
+import AoC.Parsing (Parser, run, sepNL)
+import Control.Applicative ((<|>))
+import Control.Applicative.Combinators (some)
+import Data.Bits (Bits (shiftL), (.|.))
+import Data.List (foldl', transpose)
+import Foreign (fromBool)
+import Text.Megaparsec.Char (char)
+
+mostCommonB :: [Bool] -> Bool
+mostCommonB xs = length (filter id xs) >= (length xs /^ 2)
+  where
+    -- Integer ceiling division
+    (/^) x y = (x + y - 1) `div` y
+
+leastCommonB :: [Bool] -> Bool
+leastCommonB = not . mostCommonB
+
+bsToInt :: [Bool] -> Int
+bsToInt = foldl' f 0
+  where
+    f i b = fromBool b .|. (i `shiftL` 1)
+
+findWith :: ([Bool] -> Bool) -> [[Bool]] -> [Bool]
+findWith crit = go 0
+  where
+    go _ [x] = x
+    go n xs = go (succ n) bitC
+      where
+        commonBits = transpose xs
+        bitC = filter (\bs -> bs !! n == crit (commonBits !! n)) xs
+
+solveA :: [[Bool]] -> Int
+solveA xs = gam * eps
+  where
+    commonBits = transpose xs
+    eps = bsToInt $ mostCommonB <$> commonBits
+    gam = bsToInt $ leastCommonB <$> commonBits
+
+solveB :: [[Bool]] -> Int
+solveB xs = oxi * co2
+  where
+    oxi = bsToInt $ findWith mostCommonB xs
+    co2 = bsToInt $ findWith leastCommonB xs
+
+binAsBs :: Parser [Bool]
+binAsBs = some $ ('1' ==) <$> (char '0' <|> char '1')
+
 day03a :: String -> String
-day03a = id
+day03a = show . solveA . run (sepNL binAsBs)
 
 day03b :: String -> String
-day03b = id
+day03b = show . solveB . run (sepNL binAsBs)
