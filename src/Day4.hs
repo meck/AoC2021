fcc87b4dafb5c86a48ea5e8dd92d65203418635c
meck <Johan@meck.se>
2021-12-05 13:13:14 +0100

    Day 4,5

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,7 +1,71 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day4 (day04a, day04b) where
 
+import AoC.Parsing (Parser, int, run)
+import Control.Applicative.Combinators (optional, sepBy1, some)
+import Data.List (foldl', transpose)
+import Text.Megaparsec.Char (char, eol, hspace, space1)
+
+data Marker = Unmarked | Marked deriving (Eq, Show)
+
+newtype BingoBoard = BB [[(Marker, Int)]] deriving (Eq, Show)
+
+parseBoard :: Parser BingoBoard
+parseBoard = BB <$> some (row <* optional eol)
+  where
+    row = hspace *> sepBy1 num hspace
+    num = (Unmarked,) <$> int
+
+isWinner :: BingoBoard -> Bool
+isWinner (BB bb) = or $ isCompleteLine <$> allLines
+  where
+    allLines = transpose bb <> bb
+    isCompleteLine xs = and $ (== Marked) . fst <$> xs
+
+markBoard :: Int -> BingoBoard -> BingoBoard
+markBoard m (BB bb) = BB $ fmap go <$> bb
+  where
+    go (_, x) | x == m = (Marked, x)
+    go b = b
+
+boardScore :: Int -> BingoBoard -> Int
+boardScore m (BB bb) = m * foldr go 0 (concat bb)
+  where
+    go (Marked, _) a = a
+    go (Unmarked, v) a = a + v
+
+-- Score for winning and state
+type State = ([Int], [BingoBoard])
+
+stepBords :: State -> Int -> State
+stepBords (winScores, s) m = foldr go (winScores, []) s
+  where
+    go b (winScores', s') =
+      let markedBoard = markBoard m b
+       in if isWinner markedBoard
+            then (boardScore m markedBoard : winScores', s')
+            else (winScores', markedBoard : s')
+
+winnerScores :: [BingoBoard] -> [Int] -> [Int]
+winnerScores bbs ms = fst (foldl' stepBords ([], bbs) ms)
+
+parseInput :: Parser ([BingoBoard], [Int])
+parseInput = do
+  m <- sepBy1 int (char ',') <* some space1
+  bbs <- sepBy1 parseBoard eol
+  pure (bbs, m)
+
 day04a :: String -> String
-day04a = id
+day04a =
+  show
+    . last
+    . uncurry winnerScores
+    . run parseInput
 
 day04b :: String -> String
-day04b = id
+day04b =
+  show
+    . head
+    . uncurry winnerScores
+    . run parseInput
--- src/Day5.hs
+++ src/Day5.hs
@@ -1,7 +1,52 @@
 module Day5 (day05a, day05b) where
 
+import AoC.Parsing (Parser, int, run, sepNL)
+import AoC.Util (group)
+import Data.List (sort)
+import Text.Megaparsec.Char (char, string)
+
+type Cord = (Int, Int)
+
+lineCords :: (Cord, Cord) -> [Cord]
+lineCords ((x1, y1), (x2, y2)) =
+  [(x1 + i * dirX, y1 + i * dirY) | i <- [0 .. len]]
+  where
+    dir a b
+      | a < b = 1
+      | a > b = -1
+      | otherwise = 0
+    len = maximum [abs (x1 - x2), abs (y1 - y2)]
+    dirX = dir x1 x2
+    dirY = dir y1 y2
+
+isStraight :: (Cord, Cord) -> Bool
+isStraight ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
+
+line :: Parser (Cord, Cord)
+line = do
+  x1 <- int <* char ','
+  y1 <- int <* string "-> "
+  x2 <- int <* char ','
+  y2 <- int
+  pure ((x1, y1), (x2, y2))
+
 day05a :: String -> String
-day05a = id
+day05a =
+  show
+    . length
+    . filter ((> 1) . length)
+    . group
+    . sort
+    . concatMap lineCords
+    . filter isStraight
+    . run (sepNL line)
 
 day05b :: String -> String
-day05b = id
+day05b =
+  show
+    . length
+    . filter ((> 1) . length)
+    . group
+    . sort
+    . concatMap lineCords
+    . run (sepNL line)
