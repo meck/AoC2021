ad7507a9b1beb7544712bad1d1c0b5aa08a2b97a
meck <Johan@meck.se>
2021-12-18 17:15:45 +0100

    Day 15,16

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,7 +1,51 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day15 (day15a, day15b) where
 
+import AoC.Parsing (intGrid, run)
+import AoC.Util (Cord, cardinalNeighbours)
+import Control.Arrow (first, second)
+import Control.Monad.Search (MonadSearch (abandon), SearchT, cost', runSearchBestT)
+import Control.Monad.State.Strict (State, evalState, gets, lift, modify)
+import Data.Foldable (asum)
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as M
+import Data.Maybe (catMaybes, fromJust)
+import Data.Monoid (Sum (Sum, getSum))
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+type Cave = Map Cord Int
+
+findCost :: Cave -> Maybe Int
+findCost cave = getSum . fst <$> evalState (runSearchBestT $ go [(0, 0)]) Set.empty
+  where
+    target = maximum $ M.keys cave
+    neighbours c = catMaybes $ (\x -> (x,) <$> (x `M.lookup` cave)) <$> cardinalNeighbours c
+    go :: [Cord] -> SearchT (Sum Int) (State (Set Cord)) [Cord]
+    go [] = abandon
+    go allSteps@(currentStep : _)
+      | currentStep == target = pure allSteps
+      | otherwise = do
+        alreadySeen <- lift $ gets (currentStep `Set.member`)
+        if alreadySeen
+          then abandon
+          else do
+            lift $ modify (Set.insert currentStep)
+            asum $ (\(l, d) -> cost' (Sum d) >> go (l : allSteps)) <$> neighbours currentStep
+
+expandGrid :: Cave -> Cave
+expandGrid grid = grid'
+  where
+    (xMax, yMax) = maximum $ M.keys grid
+    inc = fmap (\w -> if w == 9 then 1 else w + 1)
+    shiftRight = M.mapKeys (second (+ succ xMax))
+    shiftDown = M.mapKeys (first (+ succ yMax))
+    expandedRow = M.unions $ take 5 $ iterate (inc . shiftRight) grid
+    grid' = M.unions $ take 5 $ iterate (inc . shiftDown) expandedRow
+
 day15a :: String -> String
-day15a = id
+day15a = show . fromJust . findCost . run intGrid
 
 day15b :: String -> String
-day15b = id
+day15b = show . fromJust . findCost . expandGrid . run intGrid
--- src/Day16.hs
+++ src/Day16.hs
@@ -1,7 +1,109 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Day16 (day16a, day16b) where
 
+import AoC.Parsing (Parser, run)
+import Control.Applicative.Combinators (count, (<|>))
+import Data.Binary.Strict.BitGet (BitGet)
+import qualified Data.Binary.Strict.BitGet as BG
+import Data.Bits
+import qualified Data.ByteString as BS
+import Data.Char (digitToInt)
+import Data.Functor (($>))
+import Data.List (foldl')
+import Data.Word
+import Foreign (fromBool)
+import Text.Megaparsec (MonadParsec (lookAhead), eof, some)
+import Text.Megaparsec.Char (hexDigitChar)
+
+data TypeID = Sum | Prod | Mini | Maxi | LessT | GreatT | EqT deriving (Show)
+
+data PkgData = Literal Int | Operator TypeID [Packet] deriving (Show)
+
+data Packet = Packet Int PkgData deriving (Show)
+
+sumVersions :: Packet -> Int
+sumVersions (Packet v (Literal _)) = v
+sumVersions (Packet v (Operator _ subpkgs)) = v + sum (sumVersions <$> subpkgs)
+
+eval :: Packet -> Int
+eval = \case
+  (Packet _ (Literal i)) -> i
+  (Packet _ (Operator Sum ps)) -> sum $ eval <$> ps
+  (Packet _ (Operator Prod ps)) -> product $ eval <$> ps
+  (Packet _ (Operator Mini ps)) -> minimum $ eval <$> ps
+  (Packet _ (Operator Maxi ps)) -> maximum $ eval <$> ps
+  (Packet _ (Operator LessT ps)) -> checkPair (<) ps
+  (Packet _ (Operator GreatT ps)) -> checkPair (>) ps
+  (Packet _ (Operator EqT ps)) -> checkPair (==) ps
+  where
+    checkPair f [a, b] = if eval a `f` eval b then 1 else 0
+    checkPair _ _ = error "not two subpackets"
+
+pPacket :: BitGet Packet
+pPacket =
+  Packet
+    <$> (fromIntegral <$> BG.getAsWord8 3)
+    <*> pData
+
+pData :: BitGet PkgData
+pData =
+  BG.getAsWord8 3 >>= \case
+    0 -> Operator Sum <$> operatorVal
+    1 -> Operator Prod <$> operatorVal
+    2 -> Operator Mini <$> operatorVal
+    3 -> Operator Maxi <$> operatorVal
+    4 -> Literal <$> litteralVal
+    5 -> Operator GreatT <$> operatorVal
+    6 -> Operator LessT <$> operatorVal
+    7 -> Operator EqT <$> operatorVal
+    _ -> fail "Invalid type ID"
+
+litteralVal :: BitGet Int
+litteralVal = foldl' (\i b -> shiftL i 1 .|. fromBool b) 0 <$> go
+  where
+    go = do
+      isMore <- BG.getBit
+      i <- count 4 BG.getBit
+      if not isMore
+        then pure i
+        else (i <>) <$> go
+
+operatorVal :: BitGet [Packet]
+operatorVal = do
+  lengthID <- BG.getBit
+  if lengthID
+    then do
+      n <- fromIntegral <$> BG.getAsWord16 11
+      count n pPacket
+    else do
+      len <- BG.getAsWord16 15
+      foo <- BG.getLeftByteString (fromIntegral len)
+      let bla = BG.runBitGet foo subPackets
+      case bla of
+        Left err -> fail err
+        Right x -> pure x
+  where
+    subPackets :: BitGet [Packet]
+    subPackets = do
+      len <- BG.remaining
+      foo <- BG.lookAhead $ BG.getLeftByteString len
+      case BG.runBitGet foo pPacket of
+        Left _ -> pure []
+        Right _ -> (:) <$> pPacket <*> subPackets
+
+parseBITS :: String -> Packet
+parseBITS = either (error "bad parse") id . flip BG.runBitGet pPacket . BS.pack . run twoFourBitWords
+  where
+    twoFourBitWords = some $ do
+      let fourB :: Parser Word8
+          fourB = fromIntegral . digitToInt <$> hexDigitChar
+      a <- fourB
+      b <- fourB <|> lookAhead eof $> 0
+      pure $ shiftL a 4 .|. b
+
 day16a :: String -> String
-day16a = id
+day16a = show . sumVersions . parseBITS
 
 day16b :: String -> String
-day16b = id
+day16b = show . eval . parseBITS
